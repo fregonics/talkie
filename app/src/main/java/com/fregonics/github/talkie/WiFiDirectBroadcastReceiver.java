8608a979f87714b4bc1f7b6c16a898feee554440
luiz <luizgustavofcsg@homail.com>
2019-09-21 14:49:25 -0300

    Tried to find peers (tests inconclusive) - 0.0.0.3

--- app/src/main/java/com/fregonics/github/talkie/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/fregonics/github/talkie/WiFiDirectBroadcastReceiver.java
@@ -10,6 +10,7 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
     private WifiP2pManager manager;
     private WifiP2pManager.Channel channel;
     private MainActivity mainActivity;
+    private WifiP2pManager.PeerListListener mPeerListListener;
 
 
     public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, MainActivity activity) {
@@ -30,6 +31,8 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
             // Call WifiP2pManager.requestPeers() to get a list of current peers
+            manager.requestPeers(channel,mPeerListListener);
+            Log.d(WiFiDirectBroadcastReceiver.class.getSimpleName(), mPeerListListener.toString());
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
             // Respond to new connection or disconnections
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {

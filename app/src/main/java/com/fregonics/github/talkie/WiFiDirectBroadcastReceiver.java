fab516f6be590f2667133d5c860d201f09d3d6f5
luiz <luizgustavofcsg@homail.com>
2019-09-22 19:55:03 -0300

    Added detection of devices (have problems) - 0.0.0.4

--- app/src/main/java/com/fregonics/github/talkie/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/fregonics/github/talkie/WiFiDirectBroadcastReceiver.java
@@ -3,14 +3,19 @@ package com.fregonics.github.talkie;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
+
+import java.util.Collection;
 
 public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
     private WifiP2pManager manager;
     private WifiP2pManager.Channel channel;
     private MainActivity mainActivity;
     private WifiP2pManager.PeerListListener mPeerListListener;
+    private WifiP2pDeviceList mDevicesAvailable;
 
 
     public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, MainActivity activity) {
@@ -26,13 +31,25 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
         Log.d(WiFiDirectBroadcastReceiver.class.getSimpleName(), "ACTION: " + action);
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
             int wifiP2pState = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE,-1);
-            if(wifiP2pState != WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                Log.d(WiFiDirectBroadcastReceiver.class.getSimpleName(), "WIFI P2P DISABLED");
+            if(wifiP2pState == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                Log.d(WiFiDirectBroadcastReceiver.class.getSimpleName(), "WIFI P2P ENABLED");
+                manager.requestPeers(channel, new WifiP2pManager.PeerListListener() {
+                    @Override
+                    public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+                        Log.d(MainActivity.class.getSimpleName(),"PEER AVAILABLE");
+                        Log.d(MainActivity.class.getSimpleName(),wifiP2pDeviceList.toString());
+                    }
+                });
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
             // Call WifiP2pManager.requestPeers() to get a list of current peers
-            manager.requestPeers(channel,mPeerListListener);
-            Log.d(WiFiDirectBroadcastReceiver.class.getSimpleName(), mPeerListListener.toString());
+            manager.requestPeers(channel, new WifiP2pManager.PeerListListener() {
+                @Override
+                public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+                    Log.d(MainActivity.class.getSimpleName(),"PEER AVAILABLE");
+                    Log.d(MainActivity.class.getSimpleName(),wifiP2pDeviceList.toString());
+                }
+            });
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
             // Respond to new connection or disconnections
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
